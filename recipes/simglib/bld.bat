4639519c00958ae803cdc242ae0a5552c055868c
Sylvain Prigent <sylvain.prigent@inria.fr>
2021-08-12 16:02:55 +0200

    add simglibpy deploy

--- recipes/simglib/bld.bat
+++ recipes/simglib/bld.bat
@@ -1,4 +1,4 @@
-cmake .. -G "Visual Studio 14 2015" -A x64 -Dsimglib_BUILD_TOOLS=ON -DSL_USE_OPENMP=ON -Dsimglib_BUILD_CUDA=OFF
+cmake .. -G "Visual Studio 16 2019" -A x64 -Dsimglib_BUILD_TOOLS=ON -DSL_USE_OPENMP=ON -Dsimglib_BUILD_CUDA=OFF
 if errorlevel 1 exit 1
 cmake --build . --config Release
 if errorlevel 1 exit 1
